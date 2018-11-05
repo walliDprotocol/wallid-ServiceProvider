b2deae108e223e32313700c2798e95411a0f88bb
jafreis <jfigueira4@gmail.com>
2018-11-05 19:08:57 +0000

    Update welcome message. Update steps

--- src/Import.js
+++ src/Import.js
@@ -5,11 +5,11 @@ class Import extends Component {
   render() {
     return (
       <div className="container">
-      <h2>Personal Credit application</h2>
+      <h2>WalliD’s test validation service</h2>
         <br />
-        <p>Welcome to CrediBank automatic Personal Credit application.</p>
-        <p>Please follow through the application process. It will take you less then five minutes and it will provide an official credit proposal, ready to be accepted.</p>
-        <p>Your personal funding might be just 5 minutes away.</p>
+        <p>Welcome to WalliD’s test validation web app.</p>
+        <p>This website is intended to display  WalliD’s  validation use case through an app that  is fully working on top of the protocol and performs all identity validations of a certified Identity stored in a user’s wallet but is not requesting any real user data.</p>
+        <p>Follow the next steps to prove your (test) identity by decrypting it directly in the service</p>
         <br />
         <ImportForm />
       </div>
