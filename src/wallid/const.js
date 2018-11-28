a49d9b035ed722aa588b2dfcf2c6781455e09dc3
Adriano <adriano.campos@caixamagica.pt>
2018-11-28 16:15:34 +0000

    Add file with aplication and wallid protocol constants

---
+++ src/wallid/const.js
@@ -0,0 +1,7 @@
+// Wallid constants
+export const CONTRACT_ADDRESS = '0x787e5fc4773cad0c45f287bf00daca402845b1b7'
+export const DEFAULT_PASSWORD = "20THIS_WILL_USE_METAMASK_SECURITY18";
+
+// credibank constants
+export const VERIFYID_PROVIDER_URL = "https://verifyid.caixamagica.pt/api/v1/data"
+export const KYC_PROVIDER_URL = "https://wallid.io/kyc/api/v1/data"
