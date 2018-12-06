fabb9ba7916caba82aa5feedeb57ae0ca56b4d88
José Reis <jose.reis@caixamagica.pt>
2018-12-06 14:27:16 +0000

    Add banner and products images on the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
-import CreditForm from './components/CreditForm';
 
 class Home extends Component {
 
   render() {
     return (
-      <div className="container">
-        <h2>Personal Credit application</h2>
-        <br />
-        <p>Welcome to CrediBank automatic Personal Credit application.</p>
-        <p>Please follow through the application process. It will take you less then five minutes and it will provide an official credit proposal, ready to be accepted.</p>
-        <p>Your personal funding might be just 5 minutes away.</p>
-        <br />
-        <CreditForm />
+      <div>
+        <div className="banner">
+            <div className="info">
+                <p>Apply to a personal Credit and get approved in less than 2 minutes</p>
+                <button>Apply here</button>
+            </div>
+            <img src={require("./img/banner_1.png")} className="img-fluid" alt="Banner"/>
+            <div className="buttons">
+                <i class="fas fa-circle active"></i>
+                <i class="fas fa-circle"></i>
+                <i class="fas fa-circle"></i>
+                <i class="fas fa-circle"></i>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-4">
+                <div className="products">
+                    <img src={require("./img/product_1.png")} className="img-fluid" alt="Product 1"/>
+                    <p>Credibank applies WalliD validation service to its's Onboarding proccess</p>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="products">
+                    <img src={require("./img/product_2.png")} className="img-fluid" alt="Product 2"/>
+                    <p>Credibank is the only fully digital</p>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="products">
+                    <img src={require("./img/product_3.png")} className="img-fluid" alt="Product 3"/>
+                    <p>Know more about us</p>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
